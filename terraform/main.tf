58dcfbd3e8d7b14d6c32c16b606d059ac18614f8
NixonLeeDS <nixonthenlds@gmail.com>
2026-01-17 12:57:47 +0800

    Add Terraform configuration for EC2 infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "app_sg" {
+  name        = "devops-app-sg"
+  description = "Security group for devops app"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app" {
+  ami           = var.ami_id
+  instance_type = "t3.micro"
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  tags = {
+    Name = "devops-ci-cd-instance"
+  }
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+ami_id    = "ami-01dc51e87421923b6"
+key_name = "devops-ci-key"
